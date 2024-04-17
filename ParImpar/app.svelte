e481e2f40604e70280e19a747aff69c3bc65e797
Tupap1 <126369055+Tupap1@users.noreply.github.com>
2024-04-17 11:29:23 -0500

    Create app.svelte

---
+++ ParImpar/app.svelte
@@ -0,0 +1,12 @@
+<script>
+  import ParImpar from "./lib/parImpar.svelte";
+  let valor = 0 
+  const listenInput = (event) => valor = event.target.value
+</script>
+
+<main>
+  <ParImpar numero = {valor}/>
+
+  <input type="number" value={valor} on:input={listenInput}> 
+
+</main>
