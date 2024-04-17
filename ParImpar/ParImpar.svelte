8723793b1d8f9c0e01b81575281e9956dce2fe7b
Tupap1 <126369055+Tupap1@users.noreply.github.com>
2024-04-17 11:28:34 -0500

    Create ParImpar.svelte

---
+++ ParImpar/ParImpar.svelte
@@ -0,0 +1,21 @@
+<script>
+export let numero;
+let resultado;
+$: if (numero===0)
+  resultado = "El numero es neutro"
+else 
+    if (numero%2===0)
+      resultado = "El numero es par";
+    else
+      resultado = "El numero es impar";
+</script>
+
+<p>{resultado}</p>
+
+<style>
+  p{
+    color: antiquewhite;
+    font-family: sans-serif;
+    font-size: 25px;
+  }
+</style>
