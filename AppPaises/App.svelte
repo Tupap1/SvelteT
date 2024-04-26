b173f57678a8706bab63a32d07318c5c05c9ddb7
Tupap1 <126369055+Tupap1@users.noreply.github.com>
2024-04-26 04:50:57 -0500

    Add files via upload

---
+++ AppPaises/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Paises from "./lib/Paises.svelte";
+  const lista = ["Colombia", "Venezuela", "Ecuador", "Peru", "Brasil", "Argentina", "Bolivia", "Chile", "Guayna", "Paraguay", "Surinam", "Uruguay"]
+</script>
+
+<div class="main">
+  {#each lista as pais}
+
+  <div>
+    <Paises {pais}/>
+  </div>
+
+  {:else}
+  no existen elementos
+  {/each}
+</div>
+
+<svelte:head>
+    <title>Paises de Suramerica</title> 
+</svelte:head>
+
+<style>
+  .main {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 10px;
+        grid-auto-rows: minmax(100px, auto);
+        }
+</style>
